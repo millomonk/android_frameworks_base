d539b90c92817445ad19d3422c55c821d08bc4d0
aaronpoweruser <atlrudeboi1@gmail.com>
2013-08-24 22:09:40 +0200

    Halo: tablet mode support
    Change-Id: Iba22c4b50ed25097d7f0591f5a260d2251cd6222

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -31,6 +31,7 @@ import com.android.systemui.recent.TaskDescription;
 import com.android.systemui.statusbar.policy.NotificationRowLayout;
 import com.android.systemui.statusbar.policy.PieController;
 import com.android.systemui.statusbar.tablet.StatusBarPanel;
+import com.android.systemui.statusbar.tablet.TabletTicker;
 
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
@@ -145,6 +146,7 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     // Halo
     protected Halo mHalo = null;
+    protected TabletTicker mTabletTicker;
     protected Ticker mTicker;
     protected boolean mHaloEnabled;
     protected boolean mHaloActive;
@@ -210,6 +212,10 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     public Ticker getTicker() {
         return mTicker;
+    }
+
+    public TabletTicker getTabletTicker() {
+        return mTabletTicker;
     }
 
     public IStatusBarService getService() {
