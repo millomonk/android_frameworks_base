c154b69f2307f0e4dcd465f298d18405a9ac5b49
Schubi <schubi@erlangen.ccc.de>
2013-09-10 17:35:38 +0200

    Make BatteryBar respect lights out mode
    [log]
    
    Change-Id: I1e8317a7c8afb91cd75c4d1906e37c8b3f238921

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2472,6 +2472,7 @@ public class PhoneStatusBar extends BaseStatusBar {
             final View dockBattery = mStatusBarView.findViewById(R.id.dock_battery);
             final View dockBattery2 = mStatusBarView.findViewById(R.id.dock_battery_text);
             final View dockBattery3 = mStatusBarView.findViewById(R.id.circle_dock_battery);
+            final View batterybar = mStatusBarView.findViewById(R.id.battery_bar);
             final View clock = mStatusBarView.findViewById(R.id.clock);
             final View cclock = mStatusBarView.findViewById(R.id.center_clock);
             final View traffic = mStatusBarView.findViewById(R.id.traffic);
@@ -2500,6 +2501,9 @@ public class PhoneStatusBar extends BaseStatusBar {
             if (cclock != null) {
                 lightsOutObjs.add(ObjectAnimator.ofFloat(cclock, View.ALPHA, 0.5f));
             }
+            if (batterybar != null) {
+                lightsOutObjs.add(ObjectAnimator.ofFloat(batterybar, View.ALPHA, 0.5f));
+            }
 
             List<ObjectAnimator> lightsOnObjs = new ArrayList<ObjectAnimator>();
             lightsOnObjs.add(ObjectAnimator.ofFloat(notifications, View.ALPHA, 1));
@@ -2524,6 +2528,9 @@ public class PhoneStatusBar extends BaseStatusBar {
             }
             if (cclock != null) {
                 lightsOnObjs.add(ObjectAnimator.ofFloat(cclock, View.ALPHA, 1));
+            }
+            if (batterybar != null) {
+                lightsOutObjs.add(ObjectAnimator.ofFloat(batterybar, View.ALPHA, 1));
             }
 
             final AnimatorSet lightsOutAnim = new AnimatorSet();
