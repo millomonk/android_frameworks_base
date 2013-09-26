819f784c2951ff541e8e14c8f76157db599361a2
nuclearmistake <nuclearmistake@gmail.com>
2013-09-26 09:38:50 -0400

    Active display: Make GlowPadView center the current notification icon for us
    sudo makemeasandwich
    
    ps2: make scaling both smarter and stupider SIMULTANEOUSLY
    
    Change-Id: I582f387fd3c1c83f2bbd0d31a4b43e22d1b82d63
    Signed-off-by: nuclearmistake <nuclearmistake@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -116,7 +116,6 @@ public class ActiveDisplayView extends FrameLayout {
     private GlowPadView mGlowPadView;
     private View mRemoteView;
     private View mClock;
-    private ImageView mCurrentNotificationIcon;
     private FrameLayout mRemoteViewLayout;
     private FrameLayout mContents;
     private ObjectAnimator mAnim;
@@ -226,7 +225,6 @@ public class ActiveDisplayView extends FrameLayout {
         }
 
         public void onReleased(final View v, final int handle) {
-            ObjectAnimator.ofFloat(mCurrentNotificationIcon, "alpha", 1f).start();
             doTransition(mOverflowNotifications, 1.0f, 0);
             if (mRemoteView != null) {
                 ObjectAnimator.ofFloat(mRemoteView, "alpha", 0f).start();
@@ -240,7 +238,6 @@ public class ActiveDisplayView extends FrameLayout {
             // prevent the ActiveDisplayView from turning off while user is interacting with it
             cancelTimeoutTimer();
             restoreBrightness();
-            ObjectAnimator.ofFloat(mCurrentNotificationIcon, "alpha", 0f).start();
             doTransition(mOverflowNotifications, 0.0f, 0);
             if (mRemoteView != null) {
                 ObjectAnimator.ofFloat(mRemoteView, "alpha", 1f).start();
@@ -461,7 +458,6 @@ public class ActiveDisplayView extends FrameLayout {
 
         mRemoteViewLayout = (FrameLayout) contents.findViewById(R.id.remote_content_parent);
         mClock = contents.findViewById(R.id.clock_view);
-        mCurrentNotificationIcon = (ImageView) contents.findViewById(R.id.current_notification_icon);
 
         mOverflowNotifications = (LinearLayout) contents.findViewById(R.id.keyguard_other_notifications);
         mOverflowNotifications.setOnTouchListener(mOverflowTouchListener);
@@ -1016,7 +1012,10 @@ public class ActiveDisplayView extends FrameLayout {
         } catch (Resources.NotFoundException nfe) {
             mNotificationDrawable = mContext.getResources().getDrawable(R.drawable.ic_ad_unknown_icon);
         }
-        mCurrentNotificationIcon.setImageDrawable(mNotificationDrawable);
+        TargetDrawable centerDrawable = new TargetDrawable(getResources(),createCenterDrawable(mNotificationDrawable));
+            centerDrawable.setScaleX(0.9f);
+            centerDrawable.setScaleY(0.9f);
+            mGlowPadView.setCenterDrawable(centerDrawable);
         setHandleText(sbn);
         mNotification = sbn;
         mGlowPadView.post(new Runnable() {
@@ -1079,6 +1078,13 @@ public class ActiveDisplayView extends FrameLayout {
         stateListDrawable.addState(TargetDrawable.STATE_INACTIVE, handle);
         stateListDrawable.addState(TargetDrawable.STATE_ACTIVE, handle);
         stateListDrawable.addState(TargetDrawable.STATE_FOCUSED, handle);
+        return stateListDrawable;
+    }
+
+    private Drawable createCenterDrawable(Drawable handle) {
+        StateListDrawable stateListDrawable = new StateListDrawable();
+        stateListDrawable.addState(TargetDrawable.STATE_INACTIVE, handle);
+        
         return stateListDrawable;
     }
 
